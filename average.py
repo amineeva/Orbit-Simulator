19d96c5025dc70251b2b2303fd51d0df2b72e044
Alex Mineeva <amineeva@olin.edu>
2025-03-24 09:08:38 -0400

    Example function file - for unit test practice

---
+++ average.py
@@ -0,0 +1,11 @@
+def average_value(numbers):
+    """
+    Return the average of a list of numbers.
+
+    Args:
+        numbers: A list of numbers (ints or floats)
+    
+    Returns:
+        A float representing the average value of the numbers in the list.
+    """
+    return sum(numbers) / 3
