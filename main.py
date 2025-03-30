848d722be3578a1a34b0d4dbd8f29c8eac8368a4
Alex Mineeva <amineeva@olin.edu>
2025-03-30 18:04:45 -0400

    test planet mars, trying to add phobos moon

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 # Run project from this file
-import orbital_system_sim
+from orbital_system_sim import Planet
+from orbital_system_sim import Satellite
 import visualization
 
+# trying to create a Mars instance
+mars_planet = Planet("Mars", 3390, 6.4191*10**23, 0, 0, 0, 1.5, "rocky")
+print(mars_planet)
+
+phobos_moon = Satellite("Phobos", 11, 0, 0, 0, 0, 6000, 100, "asteroid")
+
+mars_planet.add_moon(phobos_moon)
+
+print("")
+print(mars_planet.moon_list)
