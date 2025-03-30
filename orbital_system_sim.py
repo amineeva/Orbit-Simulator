900ea7b9d831d5021e21178735dbdc22be402abc
Alex Mineeva <amineeva@olin.edu>
2025-03-30 12:57:47 -0400

    added initial classes

--- orbital_system_sim.py
+++ orbital_system_sim.py
@@ -1,1 +1,41 @@
 # Creates orbit objects, runs orbital simulation
+
+class SpaceObject:
+    """
+    Represents an orbital object in space. Has subclasses Star, Planet, Satellite.
+
+    Attributes:
+        radius: A float representing the radius of the object (all abstracted as spheres).
+        mass: A float representing the mass of the object. 
+    """
+
+    def __innit__(self, radius, mass):
+        self.radius = radius
+        self.mass = mass
+    
+
+class Star(SpaceObject):
+    """
+    Representation of a star (central point of orbit).
+    """
+
+
+class Planet(SpaceObject):
+    """
+    Representation of a planet (orbits center)
+    """
+
+class Satellite(SpaceObject): 
+    """
+    Representation of a space satellite. 
+    """
+
+class StellarOrbitalSystem:
+    """
+    Composition class, represents orbital system.
+
+    Attributes:
+        star: A Star object representing the center of the orbit.
+    """
+    def __innit__(self, star):
+        self.star = star
