b3060cd51c348481d1b7d680d5050850e97b6496
Alex Mineeva <amineeva@olin.edu>
2025-03-30 19:54:28 -0400

    set up a test mars-mercury-sun system to test functions

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 # Run project from this file
 from orbital_system_sim import Planet
 from orbital_system_sim import Satellite
+from orbital_system_sim import Star
+from orbital_system_sim import PlanetaryOrbitalSystem
+from orbital_system_sim import StellarOrbitalSystem
 import visualization
 
-# trying to create a Mars instance
+# creating planets
+mercury_planet = Planet("Mercury", 100, 100, 0, 0, 0, 0.3, "rocky")
 mars_planet = Planet("Mars", 3390, 6.4191*10**23, 0, 0, 0, 1.5, "rocky")
-print(mars_planet)
 
+# creating Martian planet moons as satellite instances
 phobos_moon = Satellite("Phobos", 11, 0, 0, 0, 0, 6000, 100, "asteroid")
+deimos_moon = Satellite("Deimos", 11, 0, 0, 0, 0, 6000, 100, "asteroid")
 
-mars_planet.add_moon(phobos_moon)
+# creating Mars orbit system 
+mars_system = PlanetaryOrbitalSystem("Mars system", mars_planet)
+mars_system.add_moon(phobos_moon)
+mars_system.add_moon(deimos_moon)
 
-print("")
-print(mars_planet.moon_list)
+# creating Sun
+sun = Star("Sun", 695700, 1.989e30, 0, 0, 0, 3.828e26, "O-type")
+
+# creating solar system
+solar_system = StellarOrbitalSystem("Solar System", sun)
+solar_system.add_orbiting_object(mars_system)
+solar_system.add_orbiting_object(mercury_planet)
+
+print(solar_system)
+print(solar_system.orbiting_objects_list())
+
+
+
